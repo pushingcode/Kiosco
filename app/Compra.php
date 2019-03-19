c2f1c9b25564957da88223417b7287488171d85d
pushingcode <code_dev@zoho.com>
2019-03-19 13:20:07 -0400

    version 3 para compras

---
+++ app/Compra.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\SoftDeletes;
+//use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Compra extends Model
+{
+    //
+    use SoftDeletes;
+    use LogsActivity;
+
+    protected $table = 'compras';
+
+    protected $dates = [
+        'created_at',
+        'deleted_at'
+      ];
+
+    //guardamos solo el evento delete 01340195161953038912  25942169
+    protected static $recordEvents = ['deleted'];
+    protected static $logName = 'warning';
+}
